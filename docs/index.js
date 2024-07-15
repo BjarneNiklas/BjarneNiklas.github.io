e9c41cab4e70571747d6f18d8ffb718b656601d3
Bjarne Niklas Luttermann <77073836+BjarneNiklas@users.noreply.github.com>
2024-07-15 08:42:55 +0200

    Delete docs directory

--- docs/index.js
+++
@@ -1,9 +0,0 @@
-const iface = gr.Interface.autoLoad({
-    element: "#interface",
-    model: window.model,
-    inputs: gr.textbox(),
-    outputs: gr.textbox(),
-    live: true,
-    theme: 'compact',
-    title: 'LSTM Wortvorhersage'
-});
